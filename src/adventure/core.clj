2cb38b67fcffdd0860f1ee645a7b1ee88f796d8e
Apoorva Aditya <apoorvaufo3@gmail.com>
2021-12-16 02:07:32 -0600

    fixed indentation

--- src/adventure/core.clj
+++ src/adventure/core.clj
@@ -1,10 +1,10 @@
 (ns adventure.core
-  (:gen-class)
-  (:require adventure.initial adventure.sketch)
-  (:refer adventure.initial)
-  (:refer adventure.sketch))
+    (:gen-class)
+    (:require adventure.initial adventure.sketch)
+    (:refer adventure.initial)
+    (:refer adventure.sketch))
 
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+    "I don't do a whole lot ... yet."
+    [& args]
+    (println "Hello, World!"))
--- src/adventure/sketch.clj
+++ src/adventure/sketch.clj
@@ -1,23 +1,23 @@
 (ns adventure.sketch
-  (:require [quil.core :as q])
-  (:require adventure.constants)
-  (:refer adventure.constants)
-  (:require adventure.room)
-  (:refer adventure.room)
-  (:require adventure.initial) ;; Only for init-state to test
-  (:refer adventure.initial))  ;; Only for init-state to test
+    (:require [quil.core :as q])
+    (:require adventure.constants)
+    (:refer adventure.constants)
+    (:require adventure.room)
+    (:refer adventure.room)
+    (:require adventure.initial) ;; Only for init-state to test
+    (:refer adventure.initial))  ;; Only for init-state to test
 
 (defn setup []
-  (q/background 200))
+    (q/background 200))
 
 (defn draw []
-  (q/background 255 0 0)
-  (q/no-stroke)
-  (draw-room init-state))   ;; Change this to current state
+    (q/background 255 0 0)
+    (q/no-stroke)
+    (draw-room init-state))   ;; Change this to current state
 
 (q/defsketch adventure
-  :title "Adventure"
-  :settings #(q/smooth 2)
-  :setup setup
-  :draw draw
-  :size [window-width window-height])
+    :title "Adventure"
+    :settings #(q/smooth 2)
+    :setup setup
+    :draw draw
+    :size [window-width window-height])
