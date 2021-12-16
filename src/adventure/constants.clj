f532c5dbc3c2acb741c4d5f9c270913083bb4a60
Apoorva Aditya <apoorvaufo3@gmail.com>
2021-12-16 15:27:42 -0600

    added constants for colors and text-field

--- src/adventure/constants.clj
+++ src/adventure/constants.clj
@@ -1,8 +1,13 @@
 (ns adventure.constants)
 
-(def window-width 1000)
-(def window-height 800)
+(def window-width 500)
+(def window-height 400)
+
+(def background-color 0)
 
 (def wall-width 50)
-
 (def door-width 100)
+(def wall-color [255 255 255])
+
+(def text-field-height 30)
+(def text-field-padding 10)
