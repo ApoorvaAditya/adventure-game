b4c56dd055cec603935386c91973ae30912866b4
Apoorva Aditya <apoorvaufo3@gmail.com>
2021-12-17 01:18:26 -0600

    added take and drop to excludes

--- src/adventure/core.clj
+++ src/adventure/core.clj
@@ -1,6 +1,6 @@
 (ns adventure.core
     (:gen-class)
-    (:refer-clojure :exclude [update])
+    (:refer-clojure :exclude [update take drop])
     (:use adventure.initial 
           adventure.sketch 
           adventure.interaction))
